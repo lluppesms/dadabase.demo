b7de21718707c288977aaf4bb76e7c305ab90d7d
Lab Luppes <labs@luppes.com>
2026-01-13 10:08:40 -0600

    Update run sql patch scripts

---
+++ src/sql.database/Patch/Patch-20260101.sql
@@ -0,0 +1,8 @@
+BEGIN TRAN
+
+INSERT INTO Joke (JokeCategoryTxt, JokeTxt) VALUES
+  ('Facts', 'Humans with two legs have an above average number of legs, compared to the entire population.')
+
+Select * From JOke Where JokeTxt like '%average number%'
+
+ROLLBACK TRAN
---
+++ src/sql.database/Patch/Patch-20260103.sql
@@ -0,0 +1,8 @@
+BEGIN TRAN
+
+INSERT INTO Joke (JokeCategoryTxt, JokeTxt) VALUES
+  ('Random', 'What do you call a Jedi with an anxiety disorder?  P-anakin Skywalker')
+
+Select * From Joke Where JokeTxt like '%Skywalker%'
+
+ROLLBACK TRAN
